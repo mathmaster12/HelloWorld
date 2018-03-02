554924d31573b18c0f1bcc449c1f54d1d88defbc
mathmaster12 <mrkorringa@gmail.com>
2018-03-02 09:14:28 -0700

    changed the Hello World message

--- src/HelloWorld2.java
+++ src/HelloWorld2.java
@@ -1,6 +1,6 @@
 
 public class HelloWorld2 {
 	public static void main(String args[]){
-		System.out.println("Hello World take 2");
+		System.out.println("Hello World take 2 the last chapter");
 	}
 }
